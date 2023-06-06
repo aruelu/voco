ced40177881274507b344abdfb055048fcc828ae
aruelu <arugacchi@gmail.com>
2023-06-06 15:07:16 +0900

    起動ランレベルの修正

--- setup.sh
+++ setup.sh
@@ -6,11 +6,12 @@ sudo pip3 install evdev
 
 sudo ln -s /home/volumio/voco/run_voco.sh /etc/init.d/run_voco.sh
 #sudo ln -s /etc/init.d/run_voco.sh /etc/rc1.d/S01run_voco.sh
-sudo ln -s /etc/init.d/run_voco.sh /etc/rc2.d/S01run_voco.sh
+#sudo ln -s /etc/init.d/run_voco.sh /etc/rc2.d/S01run_voco.sh
 #sudo ln -s /etc/init.d/run_voco.sh /etc/rc3.d/S01run_voco.sh
 #sudo ln -s /etc/init.d/run_voco.sh /etc/rc4.d/S01run_voco.sh
-#sudo ln -s /etc/init.d/run_voco.sh /etc/rc5.d/S01run_voco.sh
+sudo ln -s /etc/init.d/run_voco.sh /etc/rc5.d/S01run_voco.sh
 sudo rm -f /etc/rc1.d/S01run_voco.sh
+sudo rm -f /etc/rc2.d/S01run_voco.sh
 sudo rm -f /etc/rc3.d/S01run_voco.sh
 sudo rm -f /etc/rc4.d/S01run_voco.sh
-sudo rm -f /etc/rc5.d/S01run_voco.sh
+#sudo rm -f /etc/rc5.d/S01run_voco.sh
